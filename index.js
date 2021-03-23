d0cc19e4e0ac5c70f148b3b839333e8058a335b5
Venika <venika.jarvis@gmail.com>
2021-03-22 20:51:40 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()* 10) + 1;
+
+
+let num3 = 39
+let num4 = 5
+let mod = num3 % num4
+
+let num5 = 5
+let num6 = 10
+let num7 = 15
+let num8 = 20
+let max = Math.max(num5, num6, num7, num8);
